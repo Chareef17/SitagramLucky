b2f3ffa01be8b995a9c9827cd439e51a91ebd225
Chareef <chareef@Chareefs-MacBook-Pro.local>
2023-11-25 11:32:31 +0700

    first commit

---
+++ Sitagram/SitagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitagramApp.swift
+//  Sitagram
+//
+//  Created by Chareef on 2/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SitagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
